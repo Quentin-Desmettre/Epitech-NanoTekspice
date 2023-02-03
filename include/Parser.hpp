f4d26d249b4e0e291ee3fb15b2892e9482dea2e5
Edo <edgar.lecuyer@epitech.eu>
2023-02-03 17:46:14 +0100

    feat: add parse file

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -10,20 +10,26 @@
 #include "IComponent.hpp"
 #include "InputComponent.hpp"
 #include "OutputComponent.hpp"
+#include "ComponentFactory.hpp"
+#include <regex>
+typedef std::pair<std::vector<nts::InputComponent *>, std::vector<nts::OutputComponent *>> InputOutputPair;
 
 namespace nts {
     class Parser {
         public:
-            virtual ~Parser() = 0;
-            static std::pair<std::vector<InputComponent *>, std::vector<OutputComponent *>> parseFile(const std::string &file);
+            virtual ~Parser() = default;
+            static InputOutputPair parseFile(const std::string &file);
+            static InputOutputPair parseChipsets(std::ifstream &ifs, InputOutputPair &pair, std::map <std::string, nts::IComponent *>);
+            static void parseLinks(std::ifstream &ifs, InputOutputPair &pair, std::map <std::string, nts::IComponent *>);
 
         protected:
         private:
+            static std::regex _startChipset;
+            static std::regex _chipsetRegex;
+            static std::regex _startLink;
+            static std::regex _linkRegex;
     };
 
 } /* namespace std */
-
-
-nts::Parser::~Parser() {};
 
 #endif /* !PARSER_HPP_ */
